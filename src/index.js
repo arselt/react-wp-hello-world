a291328047d28e4311cccae6dac354330dcd304e
Arath Astorga <xarselt@gmail.com>
2022-07-17 20:17:13 -0500

    :wrench: Add config for webpack and react, html, sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import "./styles/global.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
